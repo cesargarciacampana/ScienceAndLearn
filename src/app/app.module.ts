2e36adacea9717f43bdb7951125083d41274eccd
César <cesargarciacampana@gmail.com>
2020-01-06 11:25:36 +0100

    Added menu and header component for layout Added material buttons and toolbar Added some basic css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './components/game/game.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ResolutorComponent } from './components/resolutor/resolutor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatInputModule } from '@angular/material/input';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MenuComponent } from './components/menu/menu.component';
-
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
     ResolutorComponent,
-    MenuComponent
+    MenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +30,8 @@ import { MenuComponent } from './components/menu/menu.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
