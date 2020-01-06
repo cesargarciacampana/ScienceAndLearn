a6b4ad730220a466df4878fc52a3bda35262e6e1
César <cesargarciacampana@gmail.com>
2020-01-06 10:58:08 +0100

    Added material input and basi sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { ResolutorComponent } from './components/resolutor/resolutor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MenuComponent } from './components/menu/menu.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    ResolutorComponent
+    ResolutorComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    MatInputModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
