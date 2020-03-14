ce97d502c77477a4496ab0f554c0cd93cbb4d090
César <cesargarciacampana@gmail.com>
2020-03-14 13:52:38 +0100

    Added ecuation example list Hidden balancing menu while not finished Fixed ecuation visualization

--- src/app/chemistry/components/balancing-game/balancing-game.component.ts
+++ src/app/chemistry/components/balancing-game/balancing-game.component.ts
@@ -12,7 +12,32 @@ import { EcuationHelper } from '@chem-shared/helpers/ecuation.helper';
 })
 export class BalancingGameComponent implements OnInit {
 
-  ecuation: Ecuation;
+  private ecuationStrings = [
+    'H2 + O2 = H2O',
+    'N2 + H2 = NH3',
+    'CH4 + O = CO2 + H2O',
+    'H2O + Na = NaOH + H2',
+    'KClO3 = KCl + O2',
+    'BaO2 + HCl = BaCl2 + H2O2',
+    'H2SO4 + NaCl = Na2SO4 + HCl',
+    'FeS2 = Fe3S4 + S2',
+    'H2SO4 + C = H2O + SO2 + CO2',
+    'SO2 + O2 = SO3',
+    'HCl + MnO2 = MnCl2 + H2O + Cl2',
+    'K2CO3 + C = CO + K',
+    'Ag2SO4 + NaCl = Na2SO4 + AgCl',
+    'NaNO3 + KCl = NaCl + KNO3',
+    'Fe2O3 + CO = CO2 + Fe',
+    'Na2CO3 + H2O + CO2 = NaHCO3',
+    'Cr2O3 + Al = Al2O3 + Cr',
+    'Ag + HNO3 = NO + H2O + AgNO3',
+    'CuFeS2 + O2 = SO2 + CuO + FeO',
+    'C2H6 + O2 = CO2 + H2O',
+    'FeS2 + O2 = Fe2O3 + SO2',
+    'Zn + HCl = ZnCl2 + H2',
+    'Al + HCl = AlCl3 + H2'
+  ];
+  ecuations: Ecuation[];
 
   constructor(
     private elementService : ElementService,
@@ -25,7 +50,10 @@ export class BalancingGameComponent implements OnInit {
 
   private init(){
     this.elementService.elementsObservable.subscribe(() => {
-      this.ecuation = this.ecuationHelper.parseEcuation('CH4 + O = CO2 + H2O');
+      let ecuations = [];
+      for (let i = 0; i < this.ecuationStrings.length; i++)
+        ecuations.push(this.ecuationHelper.parseEcuation(this.ecuationStrings[i]));
+      this.ecuations = ecuations;
     });
   }
 
