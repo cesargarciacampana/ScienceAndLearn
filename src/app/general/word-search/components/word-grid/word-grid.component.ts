b117add3ac51cebfca44d55680147625d96a7ef3
César <cesargarciacampana@gmail.com>
2020-10-13 23:21:23 +0200

    The words are inserted randomly into the grid

--- src/app/general/word-search/components/word-grid/word-grid.component.ts
+++ src/app/general/word-search/components/word-grid/word-grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ArrayHelper } from '@shared/helpers/array.helper';
 import { RandomHelper } from '@shared/helpers/random.helper';
+import { StringHelper } from '@shared/helpers/string.helper';
 
 @Component({
   selector: 'app-word-grid',
@@ -24,12 +25,48 @@ export class WordGridComponent implements OnInit {
     this.inicializar();
   }
 
+  isPositionValid(index: number, length: number){
+    return (index + length) % this.cols > index % this.cols;
+  }
+
+  isPositionUsed(used: Array<number>, start: number, length: number){
+    for(let i = start; i < start + length; i++)
+    {
+      if (used.indexOf(i) >= 0)
+        return true;
+    }
+    return false;
+  }
+
+  placeWords(){
+    let indexes = [];
+    let size = this.rows * this.cols;
+
+    for (let i = 0; i < this.words.length; i++){
+      let word = this.words[i];
+      let index = RandomHelper.randomIntFromInterval(0, size);
+      while (!this.isPositionValid(index, word.length)
+        || this.isPositionUsed(indexes, index, word.length)){
+        index = RandomHelper.randomIntFromInterval(0, size);
+      }
+
+      for (let j = 0; j < word.length; j++){
+        this.grid[index + j] = StringHelper.removeAccents(word[j]).toUpperCase();
+        indexes.push(index + j);
+      }
+    }
+  }
+
   inicializar(){
     let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let size = this.rows * this.cols;
+    this.grid = new Array(size);
+    
+    this.placeWords();
 
-    this.grid = [];
-    for (let i = 0; i < this.rows * this.cols; i++){
-      this.grid.push(chars[RandomHelper.randomIntFromInterval(0, chars.length)]);
+    for (let i = 0; i < size; i++){
+      if (!this.grid[i])
+        this.grid[i] = chars[RandomHelper.randomIntFromInterval(0, chars.length)];
     }
   }
 }
